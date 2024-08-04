d794a42e15b635af9280607de10c7ca2358bb8b7
Ankit Bhatia <ankit.bhatia87@gmail.com>
2024-08-04 17:05:35 +0530

    fixed horizontal scroll

--- src/features/ScrollingText/index.tsx
+++ src/features/ScrollingText/index.tsx
@@ -7,7 +7,7 @@ const ScrollingText: FC<ScrollingTextProps> = (
   const { className, children } = props;
   return (
     <div className="w-full">
-      <div className="block m-auto whitespace-nowrap overflow-hidden min-w-full h-full w-96">
+      <div className="m-auto whitespace-nowrap overflow-hidden min-w-full h-full w-[100vw] md:w-96">
         <span className={className}>{children}</span>
         <span className={className}>{children}</span>
       </div>
