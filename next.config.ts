b4e8a0821a8c3ee6edde235405837cf2b417de0a
Jarvis <jarvis@Jarviss-MacBook-Pro.local>
2026-08-09 22:07:47 +1000

    redirects to v2

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/v2",
+        permanent: true,
+      },
+    ];
+  },
   images: {
     unoptimized: false,
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
